d55ff510260a9c83fa2029b0abaebe5803776cd4
Yusuke Shimizu <shimizu.yusuke@mikilab.doshisha.ac.jp>
2025-04-25 00:57:44 +0900

    Initial commit for new repository

--- frontend/streamlit_mvp.py
+++ frontend/streamlit_mvp.py
@@ -1,3 +1,8 @@
+# frontend/streamlit_mvp.py の一番上に追加
+import os, sys
+# このファイル（streamlit_mvp.py）の親ディレクトリ (= project_root) を path に追加
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import os
 import streamlit as st
 import pandas as pd
